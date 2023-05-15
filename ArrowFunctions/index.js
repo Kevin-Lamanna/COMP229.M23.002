0f35626cad4392810b7ccd498a22598d27be3553
Kevin-Lamanna <kevin.lamanna2@gmail.com>
2023-05-15 19:44:45 -0400

    Review

--- ArrowFunctions/index.js
+++ ArrowFunctions/index.js
@@ -1,27 +1,47 @@
-// var hello;
+var hello;
 
-// hello = () => {
+// Arrow function
+hello = () => {
+  document.getElementById("demo").innerHTML += "Hello world!";//this;
+}
+
+// hello = x => {
 //   document.getElementById("demo").innerHTML += this;
 // }
 
+// hello = (x, y) => {
+//   document.getElementById("demo").innerHTML += this;
+// }
+
+// Anonymous function
+// hello = function(){
+//   document.getElementById("demo").innerHTML += this;
+// }
+
+// function sayHello(){
+//   console.log("Hello!");
+// }
+
 // //The window object calls the function:
-// window.addEventListener("load", hello);
+window.addEventListener("load", hello);
 
 // //A button object calls the function:
-// document.getElementById("btn").addEventListener("click", hello);
+document.getElementById("btn").addEventListener("click", hello);
 
 
-// var numbers = [1,2,3,4,5];
-// const squares = numbers.map(n => n * n);
-// document.write(squares);
+var numbers = [1,2,3,4,5];
+const squares = numbers.map(n => n * n);
+document.write(squares);
 
-// var evens = new Array();
-// numbers.forEach(n => {
-//    if (n % 2 === 0) { 
-//       evens.push(n);
-//    }
-// });
-// document.write(evens);
+var evens = new Array();
+numbers.forEach(n => {     // n = (n) or function (n) {}
+   if (n % 2 === 0) { 
+      evens.push(n);
+   }
+});
+document.write("<br>");
+document.write(evens);
+document.write("<br>");
 
 const author = {
     fullName: "Bob Alice",
@@ -36,7 +56,8 @@ const author = {
          document.write(book + ' by ' +this.fullName + '<br>');
       }
 
-      this.books.forEach(printValue);
+      this.books.forEach(printValue); // this.books.forEach(printValue(book));
+      // use an arrow function here to fix
     }    
 };
 author.printBooks();
--- Module/main.js
+++ Module/main.js
@@ -1,17 +1,17 @@
 // Imports the functions halfOf from the lib.js module
-import {halfOf, multiply} from './lib.js';
+// import {halfOf, multiply} from './lib.js';
 
-import doSomething from './lib.js';
+// import doSomething from './lib.js';
 
-console.log("halfOf: " + halfOf(84) );
+// console.log("halfOf: " + halfOf(84) );
 
-console.log("multiply:" + multiply(84, 2) );
+// console.log("multiply:" + multiply(84, 2) );
 
-doSomething();
+// doSomething();
 
-// import { flag, touch } from './validator.js';
-// console.log(flag); //false
-// touch(); // change to true
-// console.log(flag); // true
-// flag = false; // change to false
-// console.log(flag); // error
+import { flag, touch } from './validator.js';
+console.log(flag); //false
+touch(); // change to true
+console.log(flag); // true
+flag = false; // change to false
+console.log(flag); // error
