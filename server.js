da9e0c419a92e5768b7c52fc3ad86a3b148d7d7a
Kevin-Lamanna <kevin.lamanna2@gmail.com>
2023-05-26 21:04:40 -0400

    Update server.js

--- server.js
+++ server.js
@@ -19,6 +19,23 @@ const connect = require('connect');
 const app = connect();
 app.listen(3000);
 
+function logger(req, res, next){
+    console.log(req.method, req.url);
+    next();
+}
 
+function helloWorld(req, res, next){
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello, World!');
+}
+
+function goodbye(req, res, next){
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Goodbye!');
+}
+
+app.use(logger);
+app.use('/hello', helloWorld);
+app.use('/goodbye', goodbye);
 
 console.log('Server running at http://localhost:3000/');
