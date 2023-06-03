988dee9e4c0171bc8bf1d63e9af74135a1f7f962
Kevin-Lamanna <kevin.lamanna2@gmail.com>
2023-06-02 20:56:53 -0400

    minor changes

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.send('respond from home of the Users router');
+});
+
+/* GET users listing. */
+router.get('/about', function(req, res, next) {
+  res.send('respond from the about of the Users router');
 });
 
 module.exports = router;
