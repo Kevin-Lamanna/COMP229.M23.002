8c63d0b255df00c1f84905d9349d49f299b5e0a5
Kevin-Lamanna <kevin.lamanna2@gmail.com>
2023-06-09 20:23:14 -0400

    Update db.js

--- config/db.js
+++ config/db.js
@@ -2,18 +2,18 @@
 let atlasDB = "mongodb+srv://dbadmin:PwefQFdhYP8bDRTf@clustercomp229.1n2r5i0.mongodb.net/";
 
 // Database setup
-let {default: mongoose} = require('mongoose');
+const { default : mongoose } = require('mongoose');
 
 module.exports = function(){
 
-    mongoose.connect(atlasDB).then(() => console.log("====> Connected to MongoDB."));
+    mongoose.connect(atlasDB);
 
     let mongodb = mongoose.connection;
 
     mongodb.on('error', console.error.bind(console, 'Connection Error: '));
-    // mongodb.once('open', ()=> {
-    //     console.log("====> Connected to MongoDB.")
-    // })
+    mongodb.once('open', ()=>{
+        console.log("====> Connected to MongoDB.");
+    })
 
     return mongodb;
 
